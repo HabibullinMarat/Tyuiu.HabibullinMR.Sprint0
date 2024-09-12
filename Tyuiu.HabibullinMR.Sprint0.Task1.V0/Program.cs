ac91793d090078f851b72304e6466387ab1d7e0d
habibullinm52@gmail.com <habibullinm52@gmail.com>
2024-09-12 20:17:03 +0500

    Task1

---
+++ Tyuiu.HabibullinMR.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.HabibullinMR.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            string name = "Марат";
+            int age = 18;
+            Console.WriteLine("Здраствуйте, меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
